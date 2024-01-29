aba7dcedee3723497b1d13367f2a600159b9e76d
Nathan Faria <nathanfaria607@gmail.com>
2024-01-29 15:43:01 -0300

    feat: add view explaining the endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Create a new note'
+        },
+        {
+            'Endpoint': '/notes/id/update',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Update an existing note'
+        },
+        {
+            'Endpoint': '/notes/id/delete',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Delete an existing note'
+        }
+    ]
+
+    return JsonResponse(routes, safe=False)

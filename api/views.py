a9e1a9b0d74d4bc748fe222b6011ddecdbd243d4
Nathan Faria <nathanfaria607@gmail.com>
2024-01-29 16:26:27 -0300

    feat: add get all notes view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
 
 
 @api_view(['GET'])
@@ -38,3 +40,9 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)

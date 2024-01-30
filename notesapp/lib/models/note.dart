47ed6f49954e8a544859ce9c88f8d88c5b953ae7
Nathan Faria <nathanfaria607@gmail.com>
2024-01-30 09:50:33 -0300

    feat: add model in flutter

---
+++ notesapp/lib/models/note.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+class Note {
+  int id;
+  String note;
+  Note({
+    required this.id,
+    required this.note,
+  });
+
+  Note copyWith({
+    int? id,
+    String? note,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      note: note ?? this.note,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'body': note,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'] as int,
+      note: map['note'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromJson(String source) =>
+      Note.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Note(id: $id, note: $note)';
+
+  @override
+  bool operator ==(covariant Note other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.note == note;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ note.hashCode;
+}

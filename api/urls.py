f927504bec35193115bf6e47917ee1f5a54d4e43
Nathan Faria <nathanfaria607@gmail.com>
2024-01-30 08:08:14 -0300

    feat: add url for put view

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', view=views.getRoutes),
     path('notes/', views.getNotes),
     path('notes/create/', views.createNote),
+    path('notes/<str:pk>/update/', views.updateNote),
     path('notes/<str:pk>/', views.getNote)
 ]

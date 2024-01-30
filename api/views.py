476650372d2ede2ca574efb9271e09c43ef50534
Nathan Faria <nathanfaria607@gmail.com>
2024-01-30 08:08:02 -0300

    feat: add put note view

--- api/views.py
+++ api/views.py
@@ -63,3 +63,17 @@ def createNote(request):
 
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+
+    note = Note.objects.get(id=pk)
+
+    serializer = NoteSerializer(note, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)

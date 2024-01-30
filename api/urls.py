a02c5bf20cb52d4858fb628f1f072207ba2b6ca7
Nathan Faria <nathanfaria607@gmail.com>
2024-01-30 07:57:24 -0300

    feat: add url for post note view

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', view=views.getRoutes),
     path('notes/', views.getNotes),
+    path('notes/create/', views.createNote),
     path('notes/<str:pk>/', views.getNote)
 ]

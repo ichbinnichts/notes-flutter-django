da1901f4ac4fb2ab3b6696e1208f2427a3df62a2
Nathan Faria <nathanfaria607@gmail.com>
2024-01-31 10:32:48 -0300

    feat: add base skeleton in CreatePage

--- notesapp/lib/notes/create.dart
+++ notesapp/lib/notes/create.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class CreatePage extends StatefulWidget {
-  const CreatePage({super.key});
+  final Client client;
+
+  const CreatePage({super.key, required this.client});
 
   @override
   State<CreatePage> createState() => _CreatePageState();
 }
 
 class _CreatePageState extends State<CreatePage> {
+  TextEditingController controller = TextEditingController();
+
+  _create() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create Page'),
         backgroundColor: Colors.blue[200],
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: controller,
+          ),
+          ElevatedButton(
+            onPressed: () => _create,
+            child: const Text('Create note'),
+          )
+        ],
       ),
     );
   }

c37796bebbbb1a9f22ea37ec9b4687bad7832d99
Nathan Faria <nathanfaria607@gmail.com>
2024-01-31 11:33:49 -0300

    feat: add update page and update logic

--- notesapp/lib/notes/update.dart
+++ notesapp/lib/notes/update.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:notesapp/models/note.dart';
+import 'package:notesapp/urls.dart';
 
 class UpdatePage extends StatefulWidget {
-  const UpdatePage({super.key});
+  final Note note;
+  final Client client;
+  const UpdatePage({super.key, required this.client, required this.note});
 
   @override
   State<UpdatePage> createState() => _UpdatePageState();
 }
 
 class _UpdatePageState extends State<UpdatePage> {
+  TextEditingController controller = TextEditingController();
+
+  _update(int id) async {
+    try {
+      await widget.client.put(Uri.parse(Urls().updateNoteUrl(id)),
+          body: {'body': controller.text});
+      _returnToHomePage();
+    } catch (e) {
+      print('Error while updating note ' + e.toString());
+    }
+  }
+
+  _returnToHomePage() {
+    Navigator.pop(context);
+  }
+
+  @override
+  void initState() {
+    controller.text = widget.note.note;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update page'),
+        backgroundColor: Colors.blue[200],
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: controller,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await _update(widget.note.id);
+            },
+            child: const Text('Update note'),
+          )
+        ],
+      ),
+    );
   }
 }
